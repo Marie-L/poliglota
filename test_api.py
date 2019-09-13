36663f157bf7f19aff089bcf42ceb6fb7d945009
Marie-Louise Annan <ml_annan@hotmail.com>
2019-09-13 23:50:32 +0100

    edit typo

--- test_api.py
+++ test_api.py
@@ -1,13 +1,13 @@
 import requests
 
-
 # define class
 class test_api():
     # define function, param self refers to the class itself
 
-    def text_index(self):
-        # get request to index endpoint (local server:port)
-        r = requests.get("http://127.0.0.7:5000/")
-        # check for success status code
-        assert r.status_code == 200
+    def test_index(self):
+    # get request to index endpoint (local server:port)
+    r = requests.get("http://127.0.0.7:5000/")
+    # check for success status code
+    assert r.status_code == 200
+
 
