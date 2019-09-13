c6ec1f5d3c488910fbd4bf3e373616d2186572c1
Marie-Louise Annan <ml_annan@hotmail.com>
2019-09-13 11:57:01 +0100

    add test file

---
+++ test_api.py
@@ -0,0 +1,11 @@
+import requests
+
+# declare class
+class TestAPI():
+    # define function, param self refers to the class itself
+    def text_index(self):
+        # get request to index endpoint (local server:port)
+        r = requests.get("http://127.0.0.7:5000/")
+        # check for success status code
+        assert r.status_code == 200
+        return
